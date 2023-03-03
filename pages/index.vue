34c1863651fa0574d179b32b9ae84391df781a41
johnmartinez <johnmartinezolarte@gmail.com>
2023-03-03 13:47:26 -0500

    tercer commit

--- pages/index.vue
+++ pages/index.vue
@@ -27,11 +27,19 @@
   </div>
     <p>{{partida}}</p>
     <div class="border-l-4 border-indigo-500" ></div>
+
+ <div>
+  <canvas  class=""      ref="canvas" >
+  </canvas>
+ </div>
+
 </div>
 
 
 </template>
 <script>
+import { nextTick } from 'process'
+
   export default {
 
 
@@ -55,8 +63,11 @@
     letrascorretas:[],
     palabracor:[],
     i:0,
-    ultia:"" 
-    
+    ultia:"",
+    correctoInco:"",
+    elemento:null,
+    html:``,
+    intentos:0,
     
   
   
@@ -72,7 +83,23 @@
        this.palabraJu= this.palabras[this.randon()].message
 
        this.partida =this.palabraJu.split('')
+       this.palabracor=this.palabraJu.split('')
+       this.$refs.canvas.height = 800;
+        this.$refs.canvas.width =800;
+        const canvas = this.$refs.canvas.getContext('2d') 
+        console.log(canvas)
+  
+        canvas.beginPath();
+        canvas.fillStyle = "Red";
+        canvas.strokeStyle = "red";
+        canvas.moveTo(700, 700);
+        canvas.lineTo(100, 700);
+        canvas.moveTo(200, 700);
+        canvas.lineTo(200, 100);
+        canvas.lineTo(500, 100);
+        canvas.lineTo(500, 200);
         
+        canvas.stroke();
 
 
       },
@@ -83,33 +110,122 @@
       this.items.push( {mensaje: mensje})
     },
     letracorrect: function (letra){
+     
       this.letracorrecta=null;
-      this.partida.forEach(element => {
+      this.palabracor.forEach(element => {
         if(letra === element){
+         
         alert(element)
-        this.letracorrecta=element;
-        this.letrasColocadas.push(element)
-         this.palabracor.push(element)
+        this.correctoInco="correcto";
+        this.elemento=element;
          if(this.ultia != element){
           this. $refs[element][0].textContent = element
          }else{
           alert("se repite")
+          this.palabracor.slice( this.palabracor.indexOf(element), 1)
          }
+      
          this. $refs[element][0].textContent = element
+         this.palabracor.slice( this.palabracor.indexOf(element), 1)
          this.ultia=element;
+         console.log(this.palabracor)
+         
+      }
 
+      
+      
+        
+
+
+
+      });
+
+      if (this.correctoInco==="correcto"){
+        this.letracorrecta=this.elemento;
+        this.letrasColocadas.push(this.elemento)
+        console.log(this.letrasColocadas)
+        alert( this.correctoInco)
+      }else{
+        this.intentos++;
+
+        alert("Incorreto")
+        this.dibujar()
 
       }
-   
-      });
-      
+
+      this.correctoInco=""
     },
     suma: function (ii) {
       this.i=i+ii;
+    },
+    dibujar: function(){
+      if (this.intentos>0){
+    
+          const canvas = this.$refs.canvas.getContext('2d') 
+          canvas.beginPath();
+
+          canvas.arc(500, 250, 50, 0, Math.PI * 2, true); 
+          canvas.stroke();
+
+      }
+      if (this.intentos>1){
+    
+    const canvas = this.$refs.canvas.getContext('2d') 
+    canvas.beginPath();
+    canvas.moveTo(500, 500);
+    canvas.lineTo(500, 300);
+    canvas.stroke();
+
+}
+if (this.intentos>2){
+    
+    const canvas = this.$refs.canvas.getContext('2d') 
+    canvas.beginPath();
+    canvas.moveTo(500, 500);
+    canvas.lineTo(400, 600);
+    canvas.stroke();
+
+}
+if (this.intentos>3){
+    
+    const canvas = this.$refs.canvas.getContext('2d') 
+    canvas.beginPath();
+    canvas.moveTo(500, 500);
+    canvas.lineTo(600, 600);
+    canvas.stroke();
+
+}
+if (this.intentos>4){
+    
+    const canvas = this.$refs.canvas.getContext('2d') 
+    canvas.beginPath();
+    canvas.moveTo(500, 350);
+    canvas.lineTo(400, 400);
+    canvas.stroke();
+
+}
+if (this.intentos>4){
+    
+    const canvas = this.$refs.canvas.getContext('2d') 
+    canvas.beginPath();
+    canvas.moveTo(500, 350);
+    canvas.lineTo(600, 400);
+    canvas.stroke();
+
+}
+    },
+    colocar: function (){
+      this.palabracor.forEach(element => {
+           
+ 
+      });
     }
     }
   }
 
 </script>
+<style>
+canvas { background: #eee; display: block; margin: 0 auto; border: 1px solid black; }
+</style>
 
 
